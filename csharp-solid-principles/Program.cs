ec389c0e1405c47651ce151687455f3ddd62e920
Kouadio Brice Marcel <kbricemarcel@outlook.fr>
2025-07-09 23:02:58 +0000

    RatingEngine init

--- csharp-solid-principles/Program.cs
+++ csharp-solid-principles/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ArdalisRating
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ardalis Insurance Rating System Starting...");
+
+            var engine = new RatingEngine();
+            engine.Rate();
+
+            if (engine.Rating > 0)
+            {
+                Console.WriteLine($"Rating: {engine.Rating}");
+            }
+            else
+            {
+                Console.WriteLine("No rating produced.");
+            }
+
+        }
+    }
+}
